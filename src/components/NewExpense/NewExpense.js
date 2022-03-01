763126ae039682a5544c404004e8a5d1a200a55f
apurvagupta26 <apurvagt1@gmail.com>
2022-03-02 01:12:20 +0530

    Form opens on button click with cancel and Add new Expense functionality

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,13 @@
 import React from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense=(props)=>{
+
+const [isEditing, setIsEditing] = useState(false);
+
+
 const saveDataExpenseHandler=(enteredExpenseData)=>{
   
   const ExpenseData={
@@ -10,12 +15,33 @@ const saveDataExpenseHandler=(enteredExpenseData)=>{
     id:Math.random().toString()
   }
   console.log(ExpenseData);
+  setIsEditing(false);
  props.onAddExpense(ExpenseData)
 }
 
-return <div className="new-expense">
-    <ExpenseForm  onSaveExpenseData={saveDataExpenseHandler}/>
+const startEditingHandler = () => {
+  setIsEditing(true);
+};
+
+const stopEditingHandler = () => {
+  setIsEditing(false);
+};
+
+return ( 
+<div className="new-expense">
+    {/* <ExpenseForm  onSaveExpenseData={saveDataExpenseHandler}/> */}
+    {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )
+    }
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveDataExpenseHandler}
+          onCancel={stopEditingHandler}
+        />)
+      }
 </div>
+)
 }
 
 export default NewExpense;
